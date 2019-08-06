d49ff8bc4d31bdefc65e963097d03da4dcf06886
Punith <punith.ch@gmail.com>
2019-08-05 23:07:39 -0600

    Feat: added boiler plate for server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+
+const server = express();
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(cors());
+
+
+// Db Config
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db).then(() => {
+    console.log('MongoDB connected')
+}).catch((err) => console.log(err))
+
+
+const port = process.env.PORT || 7000
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
